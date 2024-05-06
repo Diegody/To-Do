7153cacdcd59da04997de96db8e1b83af21feba6
Diegody <diegogody12@gmail.com>
2024-05-06 16:04:19 -0500

    Arreglo de Interfaz

--- controller/db.sql
+++ controller/db.sql
@@ -1,28 +1,30 @@
--- -----------------------------------------------------
 -- Schema task
--- -----------------------------------------------------
 DROP SCHEMA IF EXISTS task;
-CREATE SCHEMA IF NOT EXISTS task DEFAULT CHARACTER SET utf8 ;
-USE task ;
+CREATE SCHEMA IF NOT EXISTS task DEFAULT CHARACTER SET utf8;
+USE task;
 
 -- Table task.Usuario
 CREATE TABLE IF NOT EXISTS task.Usuario (
-  id_user INT NOT NULL,
-  user VARCHAR(45) NOT NULL,
+  id_user INT NOT NULL AUTO_INCREMENT,
+  usuario VARCHAR(45) NOT NULL,
   nombre VARCHAR(45) NOT NULL,
-  password VARCHAR(45) NOT NULL,
-  PRIMARY KEY (id_user));
-
+  contraseña VARCHAR(45) NOT NULL,
+  creación DATETIME(0) NULL,
+  PRIMARY KEY (id_user),
+  UNIQUE KEY unique_usuario (usuario)
+);
 
 -- Table task.Tareas
 CREATE TABLE IF NOT EXISTS task.Tareas (
   id_task INT NOT NULL,
   descripcion VARCHAR(500) NOT NULL,
+  creacion DATETIME(0) NULL,
   usuario INT NOT NULL,
   PRIMARY KEY (id_task),
   INDEX fk_Tareas_Usuario_idx (usuario ASC),
   CONSTRAINT fk_Tareas_Usuario
     FOREIGN KEY (usuario)
     REFERENCES task.Usuario (id_user)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+);
+
+INSERT INTO Usuario VALUES (1, 'Diegody', 'Diego Cardenas', '1234', (SELECT SYSDATE() FROM DUAL));
