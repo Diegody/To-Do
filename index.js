e6a7758cda0fabe222b67d8902ebf4643f852b93
Diegody <diegogody12@gmail.com>
2024-05-17 18:24:30 -0500

    Cambios parciales Login

--- index.js
+++ index.js
@@ -27,17 +27,18 @@ app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(__dirname + "/view/login.html"));
 app.get('/task', (req, res) => res.sendFile(__dirname + "/view/inicio.html"));
 
+// Registrar Usuario
 app.post('/register', async (req, res) => {
     const user = req.body.user;
     const name = req.body.name;
-    const password = req.body.password;
-    let passHash = await bcryptjs.hash(password, 8);
+    const pass = req.body.password;
+    let passHash = await bcryptjs.hash(pass, 8);
 
     const query = `
         INSERT INTO Usuario (usuario, nombre, contraseña, creacion)
         VALUES (?, ?, ?, (SELECT SYSDATE() FROM DUAL))
     `;
-    
+
     connection.query(query, [user, name, passHash], (error, results, fields) => {
         if (error) {
             console.error('Error al ejecutar la consulta:', error);
@@ -45,4 +46,35 @@ app.post('/register', async (req, res) => {
         }
         res.status(200).json({ success: true, message: 'Usuario registrado con éxito' });
     });
+});
+
+// Iniciar Sesión
+app.post('/auth', async (req, res) => {
+    const { username, password } = req.body;
+
+    if (username && password) {
+        connection.query('SELECT * FROM Usuario WHERE usuario = ?', [username], async (error, results, fields) => {
+            if (error) {
+                console.error('Error al ejecutar la consulta:', error);
+                return res.status(500).json({ success: false, message: 'Error al autenticar el usuario', error: error.message });
+            }
+
+            if (results.length > 0) {
+                const user = results[0];
+                const validPassword = await bcryptjs.compare(password, user.contraseña);
+                console.log("Hash: ", password);
+                console.log("Hash: ", user.contraseña);
+
+                if (validPassword) {
+                    return res.status(200).json({ success: true, message: 'Autenticación exitosa' });
+                } else {
+                    return res.status(401).json({ success: false, message: 'Contraseña incorrecta' });
+                }
+            } else {
+                return res.status(404).json({ success: false, message: 'Usuario no encontrado' });
+            }
+        });
+    } else {
+        return res.status(400).json({ success: false, message: 'Por favor, proporciona un nombre de usuario y una contraseña' });
+    }
 });
